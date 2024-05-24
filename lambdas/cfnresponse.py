a46e7c65514adab838f59c1a6af9e897acd59e7e
salilmathur <salil@yobee.co.in>
2024-05-24 10:43:10 +0530

    cleanup - cfn response

--- lambdas/cfnresponse.py
+++ lambdas/cfnresponse.py
@@ -1,44 +1,41 @@
 from __future__ import print_function
-import urllib3
+
 import json
+import logging
+
+import requests
 
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
-http = urllib3.PoolManager()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
     responseUrl = event['ResponseURL']
 
-    print(responseUrl)
-
     responseBody = {
-        'Status' : responseStatus,
-        'Reason' : reason or "See the details in CloudWatch Log Stream: {}".format(context.log_stream_name),
-        'PhysicalResourceId' : physicalResourceId or context.log_stream_name,
-        'StackId' : event['StackId'],
-        'RequestId' : event['RequestId'],
-        'LogicalResourceId' : event['LogicalResourceId'],
-        'NoEcho' : noEcho,
-        'Data' : responseData
+        'Status': responseStatus,
+        'Reason': reason or "See the details in CloudWatch Log Stream: {}".format(context.log_stream_name),
+        'PhysicalResourceId': physicalResourceId or context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'NoEcho': noEcho,
+        'Data': responseData
     }
 
     json_responseBody = json.dumps(responseBody)
-
-    print("Response body:")
-    print(json_responseBody)
+    logger.info("CFN Response body: %s", json_responseBody)
 
     headers = {
-        'content-type' : '',
-        'content-length' : str(len(json_responseBody))
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
     }
 
     try:
-        response = http.request('PUT', responseUrl, headers=headers, body=json_responseBody)
-        print("Status code:", response.status)
-
-
+        response = requests.request('PUT', responseUrl, headers=headers, data=json_responseBody)
+        logger.info("CFN Successfully Updated")
     except Exception as e:
-
-        print("send(..) failed executing http.request(..):", e)
+        logger.info("CFN Update Failed: %s", e)
